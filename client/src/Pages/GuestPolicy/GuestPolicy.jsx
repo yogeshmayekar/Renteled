862662640f6d57ddfde33010b083cc8830207b85
Yogesh Mayekar <yogesh.mayekar09@gmail.com>
2024-02-23 01:58:43 +0530

    working on privacy policy page

--- client/src/Pages/GuestPolicy/GuestPolicy.jsx
+++ client/src/Pages/GuestPolicy/GuestPolicy.jsx
@@ -36,7 +36,7 @@ function GuestPolicy() {
                 <li>The usual standard check-in time is 12 noon. Renteled tries to ensure that you can check-in any time after that till your reservation is valid.</li>
                 <li>It is mandatory for all guests to present valid photo identification at the time of check-in. According to government regulations, a valid Photo ID has to be carried by every person above the age of 18 staying at the hotel. The identification proofs accepted are Aadhar Card, Driving License, Voter ID Card, and Passport. Note that PAN card is not acceptable. Without an original copy of a valid ID, you will not be allowed to check-in.</li>
                 <li>After reaching the hotel, if you face any difficulty in check-in and it cannot be resolved by the Hotel, you are requested to contact Renteled immediately. Renteled will verify the issue with the Hotel and post verification, you would be provided the following assistance:</li>
-                <ol type='A' className='sub_list'>
+                <ol type='a' className='sub_list'>
                     <li>Renteled will try to arrange for accommodation in the same Hotel</li>
                     <li>Renteled will try to provide you with alternate accommodation in its other listed properties if the same is available.</li>
                     <li>If Renteled is unable to provide alternative accommodation or you do not accept such alternate accommodation, you may be offered full refund.</li>
@@ -56,6 +56,46 @@ function GuestPolicy() {
                 <li>In case no cancellation policy is mentioned on the Hotel details page, the following cancellation policy shall apply:</li>
                 <p style={{padding:'5px 0', margin:'0'}}><strong>Guest cancellation policy:</strong> For booking specific cancellation policy, please refer to your booking voucher.</p>
                 <p style={{padding:'5px 0', margin:'0'}}><strong>No Show:</strong> For booking specific cancellation policy, please refer to your booking voucher.</p>
+            </ol>
+        </div>
+        <div className='long__stay'>
+            <h3>Long Stay Bookings</h3>
+            <div>
+            For bookings of more than 7 nights, you have to settle all outstanding payments on a weekly basis. Further accommodation is subject to settlement of the outstanding amount.
+            </div>
+        </div>
+        <div className='thriple__ocupency'>
+            <h3>Triple Occupancy Policy:</h3>
+            <div>Some Hotels allow triple occupancy by providing an extra mattress for the third person for extra fee. However no extra bed is usually provided.</div>
+        </div>
+        <div className='visitor__policy'>
+        <h3>Visitors Policy</h3>
+        <ol>
+            <li>You should check with the Hotel for its Visitor Policy.</li>
+            <li>In order to maintain privacy of guests and the tranquillity of the Hotel, Renteled encourages its Hotel Partners to have a visitor policy where:</li>
+            <ol type='a'>
+                <li>Visitors are generally allowed to meet guests in the guest rooms during the day, except if there is an emergency.</li>
+                <li>Visitors are not to be allowed to stay overnight.</li>
+                <li>The Hotel front desk requires all visitors to present a government approved photo identity prior to accessing guest floors/rooms.</li>
+            </ol>
+        </ol>
+        </div>
+        <div className='chield__policy'>
+            <h3>Child Policy</h3>
+            <ol>
+                <li>Stay of 1 child up to 5 years of age is complementary without the use of an extra mattress.</li>
+                <li>Breakfast charges may be applicable for the child.</li>
+            </ol>
+        </div>
+        <div className='service__on'>
+            <h3>Service On-Time policy</h3>
+            <ol>
+                <li>The hotels associated with the program will have Service On-time tag/information displayed on the app.</li>
+                <li>If you face any issue related to your stay in these hotels, we promise issue resolution within 1 hour or else we will provide refund.</li>
+                <li>If the issue can not be resolved, we will offer shifting to another room in the same hotel or shifting to a different hotel.</li>
+                <li>You need to call <strong>Renteled customer care number</strong> 7411805513 to register the issue for quick assistance.</li>
+                <li>Refund amount will be subjected to the severity of the issues and will be decided as per our guidelines.</li>
+                <li>As of now, this service is available for only selected hotels in these cities: Bengaluru, Calcutta, Delhi, Gurugram, Hyderabad, and Lucknow.</li>
             </ol>
         </div>
     </div>
