e855ad23149fbd31346d1509db74bb2e44c9035a
Yogesh Mayekar <yogesh.mayekar09@gmail.com>
2024-04-08 16:36:34 +0530

    working on review ui

--- client/src/components/bookingHistory/BookingHistory.jsx
+++ client/src/components/bookingHistory/BookingHistory.jsx
@@ -7,17 +7,19 @@ import hotel from '../../Assets/appartments.jpg';
 import axios from 'axios';
 import CloseIcon from '@mui/icons-material/Close';
 import Rating from '@mui/material/Rating';
-import TextField from '@mui/material/TextField';
 
 function BookingHistory({userId}) {
     const [age, setAge] = React.useState('confirmed');
     const [opesAddReview, setAddReview]=useState(false);
     const [bookingHotels, setBookingHotels]=useState([]);
+    const [reviewHotelId, setReviewHotelId]= useState("");
+    const [reviewerName, setReviewerName]=useState("");
     const [reviewValue, setReviewValue]=useState(1);
-    const [reviewMessage, setReviewMessage]=useState("");
+    const [reviewMessage, setReviewMessage]=useState(undefined);
+    const [disableSubmit, setDisableSunmit]=useState(true);
 
     useEffect(()=>{
-        const getBookingDataByUser =async()=>{
+        const getBookingDataByUser=async()=>{
             const res =await axios.get(`/booking/get-user-bookings/${userId}`)
             if(res.data.existingBookingByUser){
                 setBookingHotels(res.data.existingBookingByUser)
@@ -26,18 +28,44 @@ function BookingHistory({userId}) {
         getBookingDataByUser();
     },[userId])
 
+    useEffect(()=>{
+        if(reviewMessage){
+            setDisableSunmit(false)
+        }
+    },[reviewMessage])
+
+
     const isAnyBooking = bookingHotels.length>0;
 
   const handleChange = (event) => {
     setAge(event.target.value);
   }
 
-  const handleAddReview=()=>{
+  const handleAddReview=(hotelId,userName)=>{
     setAddReview(true);
+    setReviewHotelId(hotelId);
+    setReviewerName(userName);
   }
 
   const handleMessage=(e)=>{
     setReviewMessage(e.target.value);
+  }
+
+  const handleSubmitReview=async()=>{
+    console.log("hello")
+    const reviewData={
+        username:reviewerName,
+        reviewMessage:reviewMessage,
+        rating:reviewValue,
+    }
+    const res =await axios.post(`/review/${reviewHotelId}`, reviewData, {
+        withCredentials:true,
+    })
+    if(res.status===200){
+        setAddReview(false);
+        setReviewMessage("");
+        setDisableSunmit(true)
+    }
   }
 
   return (
@@ -71,7 +99,7 @@ function BookingHistory({userId}) {
             </div>
             <div className='booking__id'>
                 <h4>{data.bookingId}</h4>
-                {data.bookingStatus==='checkout' &&<button onClick={handleAddReview}>Add review</button>}
+                {data.bookingStatus==='checkout' &&<button onClick={()=>handleAddReview(data.hotelId, data.bookedByName)}>Add review</button>}
             </div>
             <div className='final_stat2'>
                  <h4>{data.bookingStatus}</h4>
@@ -97,9 +125,9 @@ function BookingHistory({userId}) {
                 />
             </div>
             <div className='input_review'>
-                <p>Add Message*</p>
+                <p>Message*</p>
                 <textarea id="message" name="message" value={reviewMessage} onChange={handleMessage} rows="4" cols="50"></textarea>
-                <button>Submit</button>
+                <button onClick={handleSubmitReview} disabled={disableSubmit} >Submit</button>
             </div>
         </div>}
         </div>
